dc04a95e8fe31ca1a71fed10150b930c0eda9f77
apradhn <abhijit.pradhan6@gmail.com>
2015-06-26 10:04:56 -0400

    all done

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require_relative 'config/environment'
 class GameApp < Sinatra::Base
   get '/rps/:play' do
     # Your app code goes here
+    @user_play = params["play"]
+    game = RPSGame.new(@user_play.to_sym)
+    @computer_play = game.computer_play
+    if game.tied?
+      @game_result = "Tie Game!"
+    elsif game.won?
+      @game_result = "You Win!"
+    elsif game.lost?
+      @game_result = "You Lose!"
+    end
     erb :rps_game
   end
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,52 @@
 class RPSGame
+  attr_reader :user_play, :computer_play
+  @@plays = [:rock, :scissors, :paper]
+
+  def initialize(user_play)
+    if self.class.valid_play?(user_play)
+      @user_play = user_play
+    else
+      raise PlayTypeError
+    end
+    @computer_play = self.class.plays.sample
+  end
+
+  def won?
+    if self.user_play == :rock && self.computer_play == :scissors
+      true
+    elsif self.user_play == :scissors && self.computer_play == :paper
+      true
+    elsif self.user_play == :paper && self.computer_play == :rock
+      true
+    else
+      false
+    end 
+  end
+
+  def tied?
+    self.user_play == self.computer_play
+  end
+
+  def lost?
+    if self.computer_play == :rock && self.user_play == :scissors
+      true
+    elsif self.computer_play == :scissors && self.user_play == :paper
+      true
+    elsif self.computer_play == :paper && self.user_play == :rock
+      true
+    else
+      false
+    end
+  end
+
+  def self.plays
+    @@plays
+  end
+
+  def self.valid_play?(play)
+    self.plays.include?(play)
+  end
+
+  class PlayTypeError < StandardError
+  end
 end
